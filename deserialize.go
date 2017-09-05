cddd140de36c47c85be55c293cbf6ee171aee354
Dmitry <dmolchan@nestlabs.com>
2017-09-04 19:12:21 -0700

    deserialize function

--- deserialize.go
+++ deserialize.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
 )
 
 // Deserialize will take a binary list
@@ -11,15 +12,54 @@ import (
 // thing wrong with tthe bianry input an error will be returned indicating such
 //
 func Deserialize(data []byte) (list List, err error) {
-	var value uint16
+	// convert our []byte to buffer that can be read
 	buffer := bytes.NewReader(data)
 
-	err = binary.Read(buffer, binary.BigEndian, &value)
-	if err != nil {
-		err = fmt.Errorf("could not read the input %v", value)
+	list = List{}
+
+	if len(data) == 0 {
+		// there was no data to iterate over
 		return
 	}
 
-	// TODO: this is only got us the first value; it also didn't make us a list
+	// we must not have empty list, lets establish the first node
+	currentNode := &Node{}
+
+	err = binary.Read(buffer, binary.BigEndian, &currentNode.Value)
+	if err != nil {
+		return
+	}
+
+	// Make the root piont to our first Node
+	list.Root = currentNode
+
+	// populate the remaining nodes in the list
+	for {
+		newNode := &Node{}
+		err = binary.Read(buffer, binary.BigEndian, &newNode.Value)
+		if err == io.EOF {
+			// we have reached the end of our buffer
+			err = nil
+			break
+		} else if err != nil {
+			// some other error occured
+			return
+		}
+
+		if newNode.Value <= currentNode.Value {
+			err = fmt.Errorf(
+				"New node value was %v, which was smaller or equal to the current node value %v",
+				newNode.Value,
+				currentNode.Value,
+			)
+			break
+		}
+
+		// The current node needs to point to the next node
+		currentNode.Next = newNode
+
+		//	We need to make the Current node the new node then repeat
+		currentNode = newNode
+	}
 	return
 }
