59f65b22ddc8e5b0c3e552cfeb3a0b8094404b4a
Dmitry <dmolchan@nestlabs.com>
2017-09-04 23:00:29 -0700

    adding deserialize tests

--- deserialize_test.go
+++ deserialize_test.go
@@ -31,8 +31,8 @@ func TestDeserialize_empty(t *testing.T) {
 	data := []byte{}
 	list, err := Deserialize(data)
 
-	if err == nil {
-		t.Fatalf("Was expecting an error but did not get one")
+	if err != nil {
+		t.Fatalf("Was not expecting an error but did not get one")
 	}
 
 	if e, a := uint(0), list.Length(); e != a {
@@ -45,6 +45,19 @@ func TestDeserialize_invalidBlob(t *testing.T) {
 		0x00, 0x05, // 5
 		0xFF, // only one byte, we are storing 16 bit integers
 	}
+
+	_, err := Deserialize(data)
+	if err == nil {
+		t.Fatalf("Was expecting an error but did not get one")
+	}
+}
+
+func TestDeserialize_unorderedBlob(t *testing.T) {
+	data := []byte{
+		0x00, 0x05, // 5
+		0x00, 0x0F, // 15
+		0x00, 0x0A, // 10
+	} // this blob is invalid as it was out of numerical order
 
 	_, err := Deserialize(data)
 	if err == nil {
